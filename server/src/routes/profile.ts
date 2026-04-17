6d8616fc6e2e271dff36ee6aad76b9501baef23e
Isaias Ogbe <ogbeisaias@gmail.com>
2026-04-17 16:39:18 -0400

    updated server

---
+++ server/src/routes/profile.ts
@@ -0,0 +1,62 @@
+import { Router, type Request, type Response } from "express";
+import { prisma } from "../lib/primsa";
+
+export const profileRouter = Router();
+
+profileRouter.post("/", async (req: Request, res: Response) => {
+    try {
+        const {userId, ...profileData } = req.body;
+
+        if (!userId) {
+            return res.status(400).json({ error: "Missing userId" });
+        }
+
+        const { 
+            goal, 
+            experience, 
+            daysPerWeek, 
+            sessionLength, 
+            equipment, 
+            injuries, 
+            preferredSplit 
+        } = profileData;
+
+        if (
+            !goal || 
+            !experience || 
+            !daysPerWeek || 
+            !sessionLength || 
+            !equipment || 
+            !injuries || 
+            !preferredSplit
+        ) {
+            return res.status(400).json({ error: "Missing required profile data" });
+        }
+        await prisma.user_profiles.upsert({ 
+            where: { user_id: userId},
+            update: {
+                goal,
+                experience,
+                days_per_week: daysPerWeek,
+                session_length: sessionLength,
+                equipment,
+                injuries: injuries || null,
+                preffered_split: preferredSplit,
+                updated_at: new Date(),
+            },
+            create: {
+                user_id: userId,
+                goal,
+                experience,
+                days_per_week: daysPerWeek,
+                session_length: sessionLength,
+                equipment,
+                injuries: injuries || null,
+                preffered_split: preferredSplit,
+            },
+    });
+    } catch (error) {
+        console.error("Error savying profile:", error);
+        res.status(500).json({ error: "Failed to save profile" });
+    }
+});
