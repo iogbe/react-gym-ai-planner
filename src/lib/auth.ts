b17dfa2b5086ebe22d6bb5751130e51c52feb1f0
Isaias Ogbe <ogbeisaias@gmail.com>
2026-04-20 15:55:46 -0400

    fixed build failure

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,4 @@
 import { createAuthClient } from '@neondatabase/neon-js/auth';
 export const authClient = createAuthClient(
-    import.meta.env.VITE_NEON_AUTH_URL,
-    {
-        redirectURI: import.meta.env.PROD 
-            ? 'https://gym-ai-frontend.onrender.com' 
-            : 'http://localhost:5173'
-    }
+    import.meta.env.VITE_NEON_AUTH_URL
 )
